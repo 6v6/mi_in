ae1cf6f6cdeffaf0b6d09a486ad035f8a6b83dc2
6v6 <dlqmslrk45>
2018-06-01 22:20:27 +0900

    수정

--- app/src/main/java/com/example/ds/homeproject/JoinActivity.java
+++ app/src/main/java/com/example/ds/homeproject/JoinActivity.java
@@ -1,9 +1,15 @@
 package com.example.ds.homeproject;
 
-import android.app.Activity;
-import android.database.sqlite.SQLiteDatabase;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+/*import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.quickstart.database.models.Post;
+import com.google.firebase.quickstart.database.models.User;*/
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -12,12 +18,16 @@ import android.widget.EditText;
 import android.widget.Spinner;
 
 public class JoinActivity extends AppCompatActivity {
+
+    /*private FirebaseDatabase Database = FirebaseDatabase.getInstance();
+    private DatabaseReference databaseReference = Database.getReference();*/
+
     String[] items = { "엄마", "아빠", "아들", "딸",};
     Button finish;
-    SQLiteDatabase db;
+
     EditText nm, id, pw, email,familyCode;
     String role;
-    boolean databaseCreated,tableCreated;
+    boolean tableCreated;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,38 +62,15 @@ public class JoinActivity extends AppCompatActivity {
         finish.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                createDatabase("home");
+
                 finish();
             }
          }
         );
     }
 
-    private void createDatabase(String name){
-        try {
-            db = openOrCreateDatabase(
-                    name,
-                    Activity.MODE_PRIVATE,
-                    null);
-            databaseCreated = true;
-            createTable("info");
-        } catch(Exception ex) {
-            ex.printStackTrace();
-        }
-    }
 
 
-    private void createTable(String name){
-        db.execSQL("create table if not exists "+name+"("
-                +"name text,"
-                +"_id text PRIMARY KEY autoincrement,"
-               +"pw text,"+"email text,"+"family text,"+"role text);");
-        tableCreated = true;
-        db.execSQL("insert into info(name,id,pw,email,family,role values("
-                +nm.getText().toString()+","+id.getText().toString()+","+pw.getText().toString()+","
-                +email.getText().toString()+","+familyCode.getText().toString()+role+");");
-
-    }
 
 
 }
